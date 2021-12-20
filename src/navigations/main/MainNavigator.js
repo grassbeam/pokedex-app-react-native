d87d9bc21e45ced95dcde1db263f2cf8b772b670
Leonardo <lcahaya@asuransiastra.com>
2021-12-20 07:21:11 +0700

    Fix AsyncStorage limit exceed on redux

--- src/navigations/main/MainNavigator.js
+++ src/navigations/main/MainNavigator.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { NavigationContainer } from '@react-navigation/native';
+
 import HomeScreen from '_screens/home/Home.Screen';
 import PokeCompare from '_screens/pokecompare/PokeCompare.Screen';
 
@@ -10,9 +11,32 @@ const Tab = createBottomTabNavigator();
 
 export default function MainNav() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Compare" component={PokeCompare} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"  
+    >
+      <Tab.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{
+          tabBarLabel: 'Pokemon List',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="pokeball" color={color} size={26} />
+          ),
+        }}
+
+      />
+      <Tab.Screen 
+        name="Compare" 
+        component={PokeCompare} 
+        options={{
+          tabBarLabel: 'Compare',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
